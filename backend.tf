2f72ee5b019f17167fb812e6f3be0a04b9b76fe1
Sai Neduri Lakshman FLT <lakshman.saineduri.ext@HYD044P25S.local>
2026-09-16 14:39:54 +0530

    Modified backend file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "lakshman-terraform-state-bucket"
-    key    = "terraform-repo"
+    key    = "terraform-base-repo"
     region = "us-east-1"
     use_lockfile = true
   }
